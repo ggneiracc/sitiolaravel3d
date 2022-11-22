6dfa048cf276669c14dc5343efff2013b9e1d584
Gonzalo Neira Calsin <ggneiracc68@gmail.com>
2022-11-22 12:09:49 -0500

    v02 examplejt1 (main) Laravel con Jetstream se cambio bootstrap

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,13 @@
 import { defineConfig } from 'vite';
 import laravel, { refreshPaths } from 'laravel-vite-plugin';
+import {resolve} from 'path';
 
 export default defineConfig({
     plugins: [
         laravel({
             input: [
-                'resources/css/app.css',
+                //'resources/css/app.css',
+                'resources/sass/app.scss',
                 'resources/js/app.js',
             ],
             refresh: [
